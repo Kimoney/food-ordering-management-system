5ddb7f37f2123e88c705c627c186dfc75feedb35
John <johntaykimani@gmail.com>
2024-04-16 04:07:58 +0300

    feat: Backend server set up.

---
+++ server/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, make_response, jsonify, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
+@app.route('/')
+def index():
+    resp = make_response({}, 200)
+    return resp
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,53 +1,60 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
-db=SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-#assosciation table storing many to many relationship between food and user
-food_users=db.Table(
-    "food_users",
-    db.Column("food_id",db.Integer,db.ForeignKey("foods.id"),primary_key=True),
-    db.Column("user_id",db.Integer,db.ForeignKey("users.id"),primary_key=True),
-)
+db = SQLAlchemy(metadata=metadata)
 
-#models
-class Food(db.Model,SerializerMixin):
-    __tablename__='foods'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(100),nullable=False)
-    #relationship mapping food to related users
-    users=db.relationship(
-        "User",
-        secondary=food_users,
-        backref="foods"
-    )
-    def __repr__(self) -> str:
-        return f"Food {self.id} {self.name}"
+
+
+# #assosciation table storing many to many relationship between food and user
+# food_users=db.Table(
+#     "food_users",
+#     db.Column("food_id",db.Integer,db.ForeignKey("foods.id"),primary_key=True),
+#     db.Column("user_id",db.Integer,db.ForeignKey("users.id"),primary_key=True),
+# )
+
+# #models
+# class Food(db.Model,SerializerMixin):
+#     __tablename__='foods'
+#     id=db.Column(db.Integer,primary_key=True)
+#     name=db.Column(db.String(100),nullable=False)
+#     #relationship mapping food to related users
+#     users=db.relationship(
+#         "User",
+#         secondary=food_users,
+#         backref="foods"
+#     )
+#     def __repr__(self) -> str:
+#         return f"Food {self.id} {self.name}"
     
     
-class User(db.Model,SerializerMixin):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(100),nullable=False)
-    contact=db.Column(db.Integer(10),nullable=False)
-    passowrd=db.Column(db.Integer(8),nullable=False)
-    #relationship mapping the users to related food
-    foods=db.relationship(
-        "Food",
-        secondary=food_users,
-        backref="users"
-    )
-    def __repr__(self) -> str:
-        return f"User {self.id} {self.name}"
+# class User(db.Model,SerializerMixin):
+#     __tablename__='users'
+#     id=db.Column(db.Integer,primary_key=True)
+#     name=db.Column(db.String(100),nullable=False)
+#     contact=db.Column(db.Integer(10),nullable=False)
+#     passowrd=db.Column(db.Integer(8),nullable=False)
+#     #relationship mapping the users to related food
+#     foods=db.relationship(
+#         "Food",
+#         secondary=food_users,
+#         backref="users"
+#     )
+#     def __repr__(self) -> str:
+#         return f"User {self.id} {self.name}"
     
-class Admin(db.Model,SerializerMixin):
-    __tablename__= 'admins'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String,)
-    contact=db.Column(db.Integer(10),nullable=False)
-    food_id=db.Column(db.Integer,db.ForeignKey('foods.id'))
-    password=db.Column(db.Integer(8),nullable=False)
-    #relattionship mapping the admin to related food
-    food=db.relationship('Food',back_populate="admins")
-    def __repr__(self) -> str:
-        return f"Admin {self.id} {self.name} {self.contact} {self.password}"
+# class Admin(db.Model,SerializerMixin):
+#     __tablename__= 'admins'
+#     id=db.Column(db.Integer,primary_key=True)
+#     name=db.Column(db.String,)
+#     contact=db.Column(db.Integer(10),nullable=False)
+#     food_id=db.Column(db.Integer,db.ForeignKey('foods.id'))
+#     password=db.Column(db.Integer(8),nullable=False)
+#     #relattionship mapping the admin to related food
+#     food=db.relationship('Food',back_populate="admins")
+#     def __repr__(self) -> str:
+#         return f"Admin {self.id} {self.name} {self.contact} {self.password}"

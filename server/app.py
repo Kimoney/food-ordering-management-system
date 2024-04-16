a6f6e552e9373607dbcf00bf76d04bb874bf2d58
John <johntaykimani@gmail.com>
2024-04-16 07:21:51 +0300

    feat: Initialized  db, added seed data and reated one api endpoint.

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db
+from models import db, User, Food, Order
 
 app = Flask(__name__)
 
@@ -20,6 +20,24 @@ api = Api(app)
 def index():
     resp = make_response({}, 200)
     return resp
+class IndexUser(Resource):
+# View all foods for the user's home/index page
+     def get(self):
+        foods = []
+        for food in Food.query.all():
+            food_dict = {
+                'id':food.id,
+                'name': food.name,
+                'category': food.category,
+                'price':food.price,
+                'ingredients': food.ingredients
+            }
+            foods.append(food_dict)
+        resp = make_response(jsonify(foods), 200)
+        return resp
+     
+     
+api.add_resource(IndexUser, '/home')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy import MetaData
+from sqlalchemy.ext.associationproxy import association_proxy
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,7 +9,54 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    serialize_rules = ('-orders.user',)
 
+    id = db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(100), unique= True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    orders = db.relationship('Order', backref='user')
+# Association proxy
+    foods = association_proxy('orders', 'food', creator=lambda food_obj: Order(food=food_obj))
+    
+    def __repr__(self):
+        return f'<User: {self.id} {self.username}>'
+    
+class Food(db.Model, SerializerMixin):
+    __tablename__ = 'foods'
+    serialize_rules = ('-orders.food',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    orders = db.relationship('Order', backref='food')
+
+    users = association_proxy('orders', 'user', creator=lambda user_obj: Order(user=user_obj))
+
+    def __repr__(self):
+        return f'<Food: {self.id} {self.name} {self.price}>'
+    
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+    serialize_rules = ('-order.users','-order.foods',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    time = db.Column(db.DateTime, server_default=db.func.now())
+    delivery_status =db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    food_id = db.Column(db.Integer, db.ForeignKey('foods.id'))
+
+    def __repr__(self):
+        return f'<Order: {self.id}  has {self.quantity} servings>'
+    
 
 # #assosciation table storing many to many relationship between food and user
 # food_users=db.Table(
